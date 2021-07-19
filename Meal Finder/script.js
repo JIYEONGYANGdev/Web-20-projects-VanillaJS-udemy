f17302db617ee6c2622f0f6bff0e9092cd63bc24
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2021-07-19 21:21:34 +0900

    Display Random Meal & Single Meal css(+relative)

--- Meal Finder/script.js
+++ Meal Finder/script.js
@@ -90,8 +90,25 @@ function addMealToDOM(meal) {
     </div>`
 }
 
+// ! Fetch random meal from API
+function getRandomMeal() {
+    // Clear meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php
+    `)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+
+            addMealToDOM(meal);
+    })
+}
+
 // * Event Listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
